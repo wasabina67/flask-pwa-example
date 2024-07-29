aa673507a6809f71dc765e9d839e5deeb087a592
wasabina67 <wasabina67@gmail.com>
2024-07-29 05:49:28 +0000

    Update run.py

---
+++ src/run.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, "static", "icons"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
